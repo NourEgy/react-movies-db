d7112abf4f1fcac2c07107b9a9554af218f28450
NourEgy <inouregy@gmail.com>
2021-10-15 20:53:40 +0200

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,8 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react'
 import useFetch from "./useFetch";
 
-
-
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 
